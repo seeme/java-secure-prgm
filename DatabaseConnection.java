4c9aaec167dc67dd4e00cb55fa2dec7e12f385bf
sammy <seeme.goo@gmail.com>
2016-09-01 00:04:15 +0800

    Add DatabaseConnection to validate IP hardcoding

---
+++ DatabaseConnection.java
@@ -0,0 +1,15 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+  
+  private String databaseServer = "127.0.0.1";
+
+  public final Connection getConnection() throws SQLException {
+    return DriverManager.getConnection("jdbc:mysql://" + databaseServer + "/dbName", 
+        "username", "password");
+  }
+}
